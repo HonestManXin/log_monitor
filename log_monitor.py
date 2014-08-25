cee9501362953f00fdf3a0727bbabe4cdb4ea59b
honestmanxin <honestmanxin@gmail.com>
2014-08-25 09:31:17 +0800

    change some processor

--- log_monitor.py
+++ log_monitor.py
@@ -6,7 +6,6 @@
 import os
 import sys
 import ConfigParser
-import stat
 import signal
 import shelve
 import time
@@ -18,13 +17,148 @@ log = logging.getLogger(__name__)
 child_pid = None
 #flag used by the subprocess determine if is killed by the parent process
 need_shutdown = False
-config = None
+global_config = None
+
+class Processor(object):
+    def __init__(self, config, filename, fd, inode, out_dir, file_index=0, pos=0):
+        self._filename = filename
+        self._current_file_fullpath = os.path.join(config["monitor_dir"], filename)
+        self._fd = fd
+        self._inode = inode
+        self._pos = pos
+        self._out_dir = out_dir
+        self._outname = filename + "." + str(file_index)
+        self._file_index = file_index
+        self._config = config
+        self._get_current_outfile_info()
+
+    def _get_current_outfile_info(self):
+        out_full_path = os.path.join(self._out_dir, self._outname)
+        if os.path.exists(out_full_path):
+            stat = os.stat(out_full_path)
+            self._out_size = stat.st_size
+        else:
+            self._out_size = 0
+        self._out_fd = open(out_full_path, 'a+')
+
+    def _write_line(line):
+        """
+            write one line log to the outname.
+        """
+        self._out_fd.write(line)
+        self._out_size += len(line)
+        self._pos = self._fd.tell()
+        if self._out_size >= self._config["zip_size"]:
+            self._file_index += 1
+            self.update_metainfo()
+            #start zip file
+            pass
+        else:
+            self.update_metainfo()
+
+    def process(self):
+        raise NotImplementedError
+
+    def update_outdir(self):
+        raise NotImplementedError
+
+    def update_metainfo(self):
+        pass
+
+class LogFileProcessor(Processor):
+    def __init__(self, *args, **kargs):
+        super(LogFileProcessor, self).__init__(*args, **kargs)
+
+    def process(self):
+        for line in self._fd:
+            self._write_line(line)
+
+        stat = os.stat(self._current_file_fullpath)
+        if stat.st_ino == self._inode:
+            return self
+        else:
+            log.warn("log file {filename} seems has been rotated".format(filename=self._filename))
+            #update the fd infos
+            return self         
+
+class RotateFileProcessor(Processor):
+    def __init__(self, *args, **kargs):
+        super(RotateFileProcessor, self).__init__(*args, **kargs)
+
+    def process(self):
+        for line in self._fd:
+            self._write_line(line)
+        log.info("rotated log file {inode} has been readed. change to real file".format(inode=self._inode))
+        real_log_filepath = os.path.join(self._config["monitor_dir"], self._filename)
+        # there may have a trival bug.
+        # when between open fd and get the inode ,log file happends rotate again.
+        fd = open(real_log_filepath, 'r')
+        inode = os.stat(real_log_filepath).st_ino
+        #
+        processor = LogFileProcessor(self._config, self._filename, fd, inode, self._out_dir, \
+            self._file_index)
+
+
+
+def is_legal_log_file(config, filename):
+    """
+    if the file is a legal log file accoding to black_list, 
+    white_list and ext_blacklist
+    """
+    if filename in config["white_list"]:
+        return True
+    if filename in config["black_list"]:
+        return False
+    for reg_exp in config["ext_blacklist"]:
+        if reg_exp.match(filename):
+            return False
+    return True
+ 
+def check_point(config):
+    """
+    check if there are files are removed.
+    """
+    pass
+
+def change_output_dir(config):
+    """
+    """
+    pass   
+
+def get_monitor_dir_information(config):
+    """
+    return file inode and file map and 
+        all the legal log file.
+    """
+    monitor_dir = config["monitor_dir"]
+    inode_filename_map = {}
+    legal_file_list = []
+    for filename in os.listdir(monitor_dir):
+        file_full_path = os.path.join(monitor_dir, filename)
+        stat = os.stat(file_full_path)
+        inode_filename_map[stat.st_ino] = filename
+        if is_legal_log_file(config, filename):
+            legal_file_list.append(filename)
+
+    return inode_filename_map, legal_file_list
+
+
+def recover_all_meta_info(config):
+    pass
+
+def update_infomation(filename, info_dict):
+    pass
 
 def run():
     """
         start monitor log_dir.
     """
-    pass    
+    meta_info = recover_all_meta_info(config)
+    inode_filename_map, legal_file_list = get_monitor_dir_information(config)
+    while not need_shutdown:
+        # consider some log file may removed situation   
+
+        #
 
 def register_subprocess_sighandler():
     """
@@ -110,20 +244,33 @@ def parse_configfile(config_file):
     config_with_section = _DummySection(config_fd)
     cp = ConfigParser.ConfigParser()
     cp.readfp(config_with_section)
-    config = {}
+    config_info = {}
     for k, v in cp.items(_dummy_section, raw=True):
-        config[k] = v
+        config_info[k] = v
     config_fd.close()
     def to_list(key, value):
         """
          convert to value to list. And the value is space delimiter.
         """
         try:
-            return re.split(r"\s+", value)
+            values = re.split(r"\s+", value)
+            values = [i for i in values if i != '']
+            return values
         except Exception, e:
             error_string = "{key}='{value}' format is not correct\n".format(key=key, value=value)
             sys.stderr.write(error_string)
-            raise e       
+            raise e
+
+    def to_end_with_reg_list(key, value):
+        """
+        convert each value to reg list.
+        """
+        values = to_list(key, value)
+        reg_list = []
+        for item in values:
+            reg_list.append(re.compile(item + r"$"))
+        return reg_list
+
     def to_int(key, value):
         """
             convert value to int.
@@ -137,19 +284,21 @@ def parse_configfile(config_file):
     parser = {
         "black_list": to_list,
         "white_list": to_list,
-        "ext_whitelist": to_list,
-        "ext_blacklist": to_list,
-        "file_size": to_int,
+        "ext_blacklist": to_end_with_reg_list,
+        "zip_size": to_int,
     }
     for each_key in parser:
-        if each_key in config:
-            config[each_key] = parser[each_key](each_key, config[each_key])
-    return config
+        if each_key in config_info:
+            config_info[each_key] = parser[each_key](each_key, config_info[each_key])
+    return config_info
 
 def check_config(config):
+    """
+        check config arguments.
+    """
     properties = ["monitor_dir", "log_dir", "meta_dir", \
-        "black_list", "white_list", "ext_whitelist", \
-        "ext_blacklist", "file_size", "rotate_time", "rotate_dir_time_format"]
+        "black_list", "white_list", "ext_blacklist", \
+        "zip_size", "rotate_dir_time", "rotate_dir_time_format"]
     config_not_exist_error = []
     for each_pro in properties:
         if config.get(each_pro, None) is None:
@@ -158,15 +307,15 @@ def check_config(config):
         sys.stderr.write("\n".join(config_not_exist_error))
         sys.exit(1)
     errors = []
-    for each_file in ["monitor_dir", "log_dir", "meta_dir"]:
-        if not os.path.exists(config[each_file]) or not os.path.isdir(config[each_file]):
-            errors.append(each_file + " does not exists or it is not a directory")
+    for each_pro in ["monitor_dir", "log_dir", "meta_dir"]:
+        if not os.path.exists(config[each_pro]) or not os.path.isdir(config[each_pro]):
+            errors.append(each_pro + " does not exists or it is not a directory")
 
-    rotate_time = ["daily", "weekly", "monthly"]
-    if not config["rotate_time"] in rotate_time:
-        errors.append("rotate_time must be " + " ".join(rotate_time))
-    if config["file_size"] < 2**20:
-        errors.append("file_size must be greater than 1M")
+    rotate_dir_time = ["daily", "weekly", "monthly"]
+    if not config["rotate_dir_time"] in rotate_dir_time:
+        errors.append("rotate_dir_time must be " + " ".join(rotate_dir_time))
+    if config["zip_size"] < 2**20:
+        errors.append("zip_size must be greater than 1M")
     if errors:
         sys.stderr.write("\n".join(errors))
         sys.exit(1)
@@ -191,12 +340,12 @@ def main():
     """
         app start function.
     """
-    global config
+    global global_config
     parser = build_option_parser()
     options = parser.parse_args()
-    config = parse_configfile(options["config_file"])
-    check_config(config)
-    build_logger(config["log_dir"], "main")
+    global_config = parse_configfile(options["config_file"])
+    check_config(global_config)
+    build_logger(global_config["log_dir"], "main")
     register_mainprocess_sighandler()
     while not need_shutdown:
         fork_worker_process()
@@ -204,5 +353,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
 
